470cf0958d92117a8a0646dc7fed19c9f935a62e
Danielhard <335805097@qq.com>
2017-09-12 22:12:05 +0800

    购物车over

--- js小项目/js/buyCar-Ajax.js
+++ js小项目/js/buyCar-Ajax.js
@@ -1,10 +1,8 @@
 (function(){
     var oBcProListCon=document.querySelector("#bcProListCon");
-    var oCountCon=document.querySelector(".countCon");
-
+    var delAll=document.querySelector(".delAll");
     var token=matchQueryString("token");
      myajax.get("http://h6.duchengjiu.top/shop/api_cart.php",{token: localStorage.token,},function(error,responseText){
-
         var json=JSON.parse(responseText);
         console.log(json);
         var data=json.data;
@@ -17,7 +15,7 @@
         </div>
         <div class="bcProListItemName fl"><a href="#">${obj.goods_name}</a></div>
         <div class="colorAndStyle fl">
-          颜色分类：B款天鹅项链（银色）
+          ${obj.goods_id}
         </div>
         <div class="unitPrice fl">&yen;<span class="unitPriceSpan">${obj.goods_price}</span></div>
         <div class="count fl">
@@ -27,15 +25,53 @@
         </div>
         <div class="singleSum fl gold">&yen;<span class="singleSumSpan">${obj.goods_price}</span></div>
         <div class="operation fl">
-          <a class="operationDel" href="javascript:(0)">删除</a>
+          <a class="operationDel" data-id="${obj.goods_id}" name="delete" href="javascript:(0)">删除</a>
         </div>
       </div>
             `;
         }
      });
-
-
-
-
-
+    oBcProListCon.addEventListener("click",function (event) {
+        event=event||window.event;
+        var target=event.target||event.srcElement;
+        if(target.name==="delete"){
+           if(!confirm("确认删除？")){
+               return;
+           }
+         var goods_id=target.dataset.id;
+         var number=0;
+           myajax.post("http://h6.duchengjiu.top/shop/api_cart.php?token="+localStorage.token,
+               {goods_id,number},function (error,responseText) {
+                var json=JSON.parse(responseText);
+                console.log(json);
+                if(json.code===0){
+                  var delDiv=target.parentNode.parentNode;
+                  delDiv.parentNode.removeChild(delDiv);
+                }
+           })
+        }
+   });
+    delAll.addEventListener("click",function () {
+       if(!confirm("确定清空整个购物车吗？")){
+           return;
+       }
+        var oGoodsDiv=document.querySelectorAll(".colorAndStyle");
+       for (var i=0;i<oGoodsDiv.length;i++){
+           var branch=oGoodsDiv[i];
+           var goods_id=parseInt(branch.innerText);
+           var number=0;
+           (function (branch) {
+               myajax.post("http://h6.duchengjiu.top/shop/api_cart.php?token="+localStorage.token,
+                   {goods_id,number},function (error,responseText) {
+                       var json=JSON.parse(responseText);
+                       console.log(json);
+                       if(json.code===0){
+                           var father=branch.parentNode;
+                           father.parentNode.removeChild(father);
+                       }
+                   })
+           })(branch)
+       }
+    })
 })()
+
