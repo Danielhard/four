8a5b64d119502852b955ce3341f5934e642159f0
Han-Lun <924853978@qq.com>
2017-09-14 19:22:39 +0800

    历史搜索

--- js小项目/js/backtotop.js
+++ js小项目/js/backtotop.js
@@ -5,25 +5,25 @@
 	this.init();
 	this.bindEvent();
 
-	// this.bindScrollEvent();
+	 this.bindScrollEvent();
 
 }
 BackToTop.prototype.init = function() {
 	this.dom = document.querySelector(this.selector);
 }
 
-// BackToTop.prototype.bindScrollEvent = function() {
-//     var self = this;
-// 	window.onscroll = function(){
-//         var scrollTop1 = document.body.scrollTop || document.documentElement.scrollTop;
-//         console.log(scrollTop1);
-//         if (scrollTop1 > 500) {
-//             self.dom.style.display = 'block';
-//         } else {
-//             self.dom.style.display = 'none';
-//         }
-//     }
-// }
+   BackToTop.prototype.bindScrollEvent = function() {
+       var self = this;
+   	window.addEventListener('scroll',function(){
+   		 var scrollTop1 = document.body.scrollTop || document.documentElement.scrollTop;
+           console.log(scrollTop1);
+           if (scrollTop1 > 500) {
+               self.dom.style.display = 'block';
+           } else {
+               self.dom.style.display = 'none';
+           }
+   	})
+   }
 
 
 
