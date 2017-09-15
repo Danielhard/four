c6860cfd7c6ca575c95feba47331c0922f2b7669
YJH <m17854239497@outlook.com>
2017-09-15 19:15:18 +0800

    未登录状态

---
+++ js小项目/js/isLogin.js
@@ -0,0 +1,34 @@
+(function(){
+  var myFiveList = document.querySelector('.myFiveList');
+  var allMyFiveList = myFiveList.querySelectorAll('a');
+  var buyCar = document.querySelector('.buyCar');
+  buyCar.addEventListener('click',function(event) {
+    event = event || window.event;
+    if(!localStorage.token){
+      event.preventDefault();
+      toast("请先登录",3000);
+      location.href='login.html';
+    } else {
+      location.href='buyCar.html';
+    }
+
+  },false);
+
+  for(var i = 0; i < allMyFiveList.length; i++){
+      allMyFiveList[i].addEventListener('click',function(event){
+        event = event || window.event;
+        var target = event.target || event.srcElement;
+        if(!localStorage.token){
+          event.preventDefault();
+          toast("请先登录",3000);
+          location.href='login.html';
+        }else{
+          if(target.innerText === '我的订单'){
+            location.href='orderPage.html';
+          }else if(target.innerText === '我的收货地址'){
+            location.href='address.html';
+          }
+        }
+      },false);
+  }
+})();
